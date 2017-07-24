2f220b06e623a3951664814b6ba38edef77c3866
Stefan Linke <particleflux@gmail.com>
2017-07-24 23:54:05 +0200

    Require alexa-sdk

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 /**
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills
  * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * This sample supports multiple languages. (en-US, en-GB, de-DE).
  * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
  * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
  **/
@@ -61,7 +61,7 @@ const handlers = {
         this.emit('GetFact');
     },
     'GetFact': function () {
-        // Get a random space fact from the space facts list
+        // Get a random fact from the facts list
         // Use this.t() to get corresponding language data
         const factArr = this.t('FACTS');
         const factIndex = Math.floor(Math.random() * factArr.length);
@@ -84,13 +84,12 @@ const handlers = {
     },
     'SessionEndedRequest': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
-    },
+    }
 };
 
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
