ceb6d035a2999c74c2acc46eb3db931c1ea3832a
Stefan Linke <particleflux@gmail.com>
2017-07-30 17:16:10 +0200

    Use webpack for deployment

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const path = require('path');
+const pkg = require('./package.json');
+
+module.exports = {
+    entry: path.resolve(__dirname, pkg.main),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        libraryTarget: 'commonjs2',
+        filename: pkg.main
+    },
+    target: 'node',
+    externals: ['aws-sdk'],
+    module: {
+        /**
+         * Tell webpack how to load JSON files.
+         * When webpack encounters a 'require()' statement
+         * where a JSON file is being imported, it will use
+         * the json-loader
+         */
+        loaders: [
+            {
+                test:/\.js$/,
+                loader:'babel-loader'
+},
+            {
+                test: /\.json$/,
+                loaders: ['json-loader']
+            }
+        ]
+    }
+};
